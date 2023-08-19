4e4604d6a259ecbeecbb9a27cf02bc82e9213d53
Douglas Henrique <dev.dhbarboza@gmail.com>
2023-08-19 20:22:30 -0300

    Add script

--- script.js
+++ script.js
@@ -61,3 +61,24 @@ const question = [
         ],
     },
 ];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementByI("answer-buttons");
+const nextButton = document.getElementByI("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let qestionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = qestionNo;
+    +". " + currentQuestion.questionName + " " + currentQuestion;
+}
