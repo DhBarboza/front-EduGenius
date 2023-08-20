2a2527d594ac504cefb98f6a94a7c1eb984c149d
Henrique Freitas <henrique.manoel@hotmail.com.br>
2023-08-20 10:47:53 -0300

    style nav

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,11 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
     <img src="./assets/LogotipoEduGenius.png">
-    <router-link to="/">Home</router-link>
-    <router-link to="/registerContent">Registar Conteudo</router-link>
-    <router-link to="/login">Login</router-link>
+    <div class="d-inline-flex p-2"> 
+      <router-link to="/" class="btn">Home</router-link>
+      <router-link to="/registerContent" class="btn">Registar Conteudo</router-link>
+      <router-link to="/login" class="btn">Login</router-link>
+    </div>
   </nav>
   <router-view />
 </template>
